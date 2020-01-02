784cae85c4bc5fe692460ba0d923498ab86e5f68
Mickael MAS <mickael.mas@appiwedia.com>
2020-01-02 12:47:34 +0100

    Initial Commit

---
+++ GitHub-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub-SwiftUI
+//
+//  Created by Mickael Mas on 02/01/2020.
+//  Copyright © 2020 APPIWEDIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub-SwiftUITests/GitHub_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_SwiftUITests.swift
+//  GitHub-SwiftUITests
+//
+//  Created by Mickael Mas on 02/01/2020.
+//  Copyright © 2020 APPIWEDIA. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_SwiftUI
+
+class GitHub_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
